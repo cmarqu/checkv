2f9d2ded7cb6592ad2abd11f6a093608c17117e4
Colin Marquardt <cmarqu42@gmail.com>
2019-02-25 01:18:15 +0100

    Change test names to be more specific.

--- tests/test_check_equal_real.py
+++ tests/test_check_equal_real.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-"""Tests for ``check_real()`` for floating point values."""
+"""Tests for ``check_equal()`` for floating point values."""
 
 # Copyright checkv contributors
 # Licensed under the Revised BSD License, see LICENSE for details.
@@ -9,7 +9,7 @@
 import checkv
 
 
-def test_check_equal_0_0_pass(caplog):
+def test_check_equal_real_0_0_pass(caplog):
     checkv.check_equal(0.0, 0.0)
     
     for record in caplog.records:
@@ -17,7 +17,7 @@ def test_check_equal_0_0_pass(caplog):
     assert "Equality check passed - Got abs (0.0 - 0.0) <= 0.0." in caplog.text
 
 
-def test_check_equal_0_1_pass(caplog):
+def test_check_equal_real_0_1_pass(caplog):
     checkv.check_equal(0.0, 1.0, max_diff=1.0)
     
     for record in caplog.records:
@@ -25,7 +25,7 @@ def test_check_equal_0_1_pass(caplog):
     assert "Equality check passed - Got abs (0.0 - 1.0) <= 1.0." in caplog.text
 
     
-def test_check_equal_0_1_fail(caplog):
+def test_check_equal_real_0_1_fail(caplog):
     checkv.check_equal(0.0, 1.0)
     
     for record in caplog.records:
@@ -33,7 +33,7 @@ def test_check_equal_0_1_fail(caplog):
     assert "Equality check failed - Got abs (0.0 - 1.0) > 0.0." in caplog.text
 
     
-def test_check_equal_0_1_fail2(caplog):
+def test_check_equal_real_0_1_fail2(caplog):
     checkv.check_equal(0.0, 1.0, max_diff=0.9)
     
     for record in caplog.records:
@@ -43,7 +43,7 @@ def test_check_equal_0_1_fail2(caplog):
 
 my_checker = "FIXME"
 
-def test_check_equal_my_checker__0_0_pass(caplog):
+def test_check_equal_real_my_checker__0_0_pass(caplog):
     checkv.check_equal(0.0, 0.0, checker=my_checker)
     
     for record in caplog.records:
@@ -51,7 +51,7 @@ def test_check_equal_my_checker__0_0_pass(caplog):
     assert "Equality check passed - Got abs (0.0 - 0.0) <= 0.0." in caplog.text
 
 
-def test_check_equal_my_checker__0_1_pass(caplog):
+def test_check_equal_real_my_checker__0_1_pass(caplog):
     checkv.check_equal(0.0, 1.0, max_diff=1.0, checker=my_checker)
     
     for record in caplog.records:
@@ -59,7 +59,7 @@ def test_check_equal_my_checker__0_1_pass(caplog):
     assert "Equality check passed - Got abs (0.0 - 1.0) <= 1.0." in caplog.text
 
     
-def test_check_equal_my_checker__0_1_fail(caplog):
+def test_check_equal_real_my_checker__0_1_fail(caplog):
     checkv.check_equal(0.0, 1.0, checker=my_checker)
     
     for record in caplog.records:
@@ -67,7 +67,7 @@ def test_check_equal_my_checker__0_1_fail(caplog):
     assert "Equality check failed - Got abs (0.0 - 1.0) > 0.0." in caplog.text
 
     
-def test_check_equal_my_checker__0_1_fail2(caplog):
+def test_check_equal_real_my_checker__0_1_fail2(caplog):
     checkv.check_equal(0.0, 1.0, max_diff=0.9, checker=my_checker)
     
     for record in caplog.records:
